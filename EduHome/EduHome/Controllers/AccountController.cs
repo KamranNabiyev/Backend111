7f041e32f0784353480057f17c12ca1e5c367e87
KamranNabiyev <>
2023-03-11 15:17:18 +0400

    Init

---
+++ EduHome/EduHome/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using EduHome.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace EduHome.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            
+
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
